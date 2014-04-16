cb04bac868f07dda95f32869ded43fd17a916417
denjo <denjo@denjo-pc.(none)>
2014-04-16 15:43:56 +0900

    first

---
+++ 3_5.c
@@ -0,0 +1,197 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void FFT( double *xr, double *xi, double *Xr, double *Xi, int N )
+{
+  int i, j, k, n, n2;
+  double theta, wr, wi;
+
+  static double *rbuf, *ibuf;
+  static int    bufsize = 0;
+
+  /* memory allocation for buffers */
+  if( bufsize != N ) {
+    bufsize = N;
+    rbuf = (double*)calloc( sizeof(double), bufsize );
+    ibuf = (double*)calloc( sizeof(double), bufsize );
+  }
+
+  /* bit reverse of xr[] & xi[] --> store to rbuf[] and ibuf[] */
+  i = j = 0 ;
+  rbuf[j] = xr[j];  ibuf[j] = xi[j];
+  for( j = 1 ; j < N-1 ; j++ ) {
+    for( k = N/2 ; k <= i ; k /= 2 )  i -= k;
+    i += k;
+    rbuf[j] = xr[i];  ibuf[j] = xi[i];
+  }
+  rbuf[j] = xr[j];  ibuf[j] = xi[j];
+
+  /* butterfly calculation */
+  theta = -2.0*M_PI;
+  for( n = 1 ; ( n2 = n*2 ) <= N ; n = n2 ) {
+    theta *= 0.5;
+    for ( i = 0 ; i < n ; i++ ) {
+      wr = cos(theta*i);  wi = sin(theta*i);
+      for ( j = i ; j < N ; j += n2 ) {
+	k = j + n;
+	/*
+	  X[j] = 1*buf[j] + W*buf[k];
+	  X[k] = 1*buf[j] - W*buf[k];
+	  Note : X[], buf[], and W are complex numbers.
+	  Re{ X[n] } = Xr[n], Im{ X[n] } = Xi[n];
+	  Re{ buf[n] } = rbuf[n], Im{ buf[n] } = ibuf[n];
+	  Re{ W } = wr, Im{ W } = wi;
+	*/
+	Xr[j] = rbuf[j] + wr*rbuf[k] - wi*ibuf[k];  /* ??????????, using wr, wi, rbuf, and ibuf */
+	Xi[j] = ibuf[j] + wr*ibuf[k] + wi*rbuf[k];  /* ??????????, using wr, wi, rbuf, and ibuf */
+	Xr[k] = rbuf[j] - wr*rbuf[k] + wi*ibuf[k];  /* ??????????, using wr, wi, rbuf, and ibuf */
+	Xi[k] = ibuf[j] - wr*ibuf[k] - wi*rbuf[k];  /* ??????????, using wr, wi, rbuf, and ibuf */
+      }
+    }
+
+    for( i = 0 ; i < N ; i++ ) {
+      rbuf[i] = Xr[i];
+      ibuf[i] = Xi[i];
+    }
+  }
+  return;
+}
+
+void IFFT( double *Xr, double *Xi, double *xr, double *xi, int N )
+{
+  int i, j, k, n, n2;
+  double theta, wr, wi;
+
+  static double *rbuf, *ibuf;
+  static int    bufsize = 0;
+
+  /* memory allocation for buffers */
+  if( bufsize != N ) {
+    bufsize = N;
+    rbuf = (double*)calloc( sizeof(double), bufsize );
+    ibuf = (double*)calloc( sizeof(double), bufsize );
+  }
+
+  /* bit reverse of Xr[] & Xi[] --> store to rbuf[] and ibuf[] */
+  i = j = 0 ;
+  rbuf[j] = Xr[j]/N;  ibuf[j] = Xi[j]/N;
+  for( j = 1 ; j < N-1 ; j++ ) {
+    for( k = N/2 ; k <= i ; k /= 2 )  i -= k;
+    i += k;
+    rbuf[j] = Xr[i]/N;  ibuf[j] = Xi[i]/N;
+  }
+  rbuf[j] = Xr[j]/N;  ibuf[j] = Xi[j]/N;
+
+  /* butterfly calculation */
+  theta = 2.0*M_PI;  /* not -2.0*M_PI !!! */
+  for( n = 1 ; ( n2 = n*2 ) <= N ; n = n2 ) {
+    theta *= 0.5;
+    for ( i = 0 ; i < n ; i++ ) {
+      wr = cos(theta*i);  wi = sin(theta*i);
+      for ( j = i ; j < N ; j += n2 ) {
+	k = j + n;
+	/*
+	  x[j] = 1*buf[j] + W*buf[k];
+	  x[k] = 1*buf[j] - W*buf[k];
+	  Note : x[], buf[], and W are complex numbers.
+	  Re{ x[n] } = xr[n], Im{ x[n] } = xi[n];
+	  Re{ buf[n] } = rbuf[n], Im{ buf[n] } = ibuf[n];
+	  Re{ W } = wr, Im{ W } = wi;
+	*/
+	xr[j] = rbuf[j] + wr*rbuf[k] - wi*ibuf[k];   /* ??????????, using wr, wi, rbuf, and ibuf */
+	xi[j] = ibuf[j] + wr*ibuf[k] + wi*rbuf[k];   /* ??????????, using wr, wi, rbuf, and ibuf */
+	xr[k] = rbuf[j] - wr*rbuf[k] + wi*ibuf[k];   /* ??????????, using wr, wi, rbuf, and ibuf */
+	xi[k] = ibuf[j] - wr*ibuf[k] - wi*rbuf[k];   /* ??????????, using wr, wi, rbuf, and ibuf */
+      }
+    }
+
+    for( i = 0 ; i < N ; i++ ) {
+      rbuf[i] = xr[i];
+      ibuf[i] = xi[i];
+    }
+  }
+  return;
+}
+
+int main( int argc, char *argv[] )
+{
+  int  i, nskip, framelen;
+  double spec;
+  short  *sdata;
+  double *xr, *xi, *Xr, *Xi;
+  FILE *fpDAT;
+  
+  if( 4 != argc ) {
+    fprintf( stderr, "Usage : %s DATfile skip[sample] frame_length[sample]\n", argv[0] );
+    exit( 1 );
+  }
+
+  if( NULL == ( fpDAT = fopen( argv[1], "r" ) ) )  exit( 1 );
+  if( 0 > ( nskip    = atoi( argv[2] ) ) )  exit( 1 );
+  if( 0 > ( framelen = atoi( argv[3] ) ) )  exit( 1 );
+
+  fprintf( stderr, "# DATfile = %s\n", argv[1] );
+  fprintf( stderr, "# %d samples are skipped.\n", nskip );
+  fprintf( stderr, "# 1 frame contains %d sampels.\n", framelen );
+
+  /* memory allocation */
+  sdata = (short*)calloc( sizeof(short), framelen );
+  xr = (double*)calloc( sizeof(double), framelen );
+  xi = (double*)calloc( sizeof(double), framelen );
+  Xr = (double*)calloc( sizeof(double), framelen );
+  Xi = (double*)calloc( sizeof(double), framelen );
+  if( NULL == sdata || NULL == xi || NULL == xi || NULL == Xr || NULL == Xi )  exit( 1 );
+
+  fseek( fpDAT, nskip*sizeof(short), SEEK_SET );
+  fread( sdata, sizeof(short), framelen, fpDAT );
+  fclose( fpDAT );
+
+  for( i = 0 ; i < framelen ; i++ ) {
+    xr[i] = (0.54-0.46*cos(2*M_PI*i/(framelen-1)))*sdata[i];
+    xi[i] = 0.0;
+  }
+
+
+  FFT( xr, xi, Xr, Xi, framelen );
+
+  for( i = 0 ; i < framelen ; i++ ) {
+    Xr[i] = log10( (1.0/framelen)*( Xr[i]*Xr[i]+Xi[i]*Xi[i] ) );
+    Xi[i] = 0;
+  }
+  
+  IFFT( Xr, Xi, xr, xi, framelen );
+  
+  /*
+  for(i = 0; i < framelen; i++){
+    printf("%d %lf\n", i, xr[i]);
+  }
+  */
+
+  for(i = 0; i < framelen; i++){
+    if(50 <= i && framelen - 50 > i){
+      xr[i] = 0;
+    }
+  }
+ 
+  FFT( xr, xi, Xr, Xi, framelen);
+  
+  for(i = 0; i < framelen; i++){
+    printf("%d %lf\n", i, Xr[i]);
+  }
+  
+  /*
+  for( i = 0 ; i < framelen ; i++ ) {
+    spec = log10( (1.0/framelen)*( Xr[i]*Xr[i]+Xi[i]*Xi[i] ) );
+    printf( "%d %lf\n", i, spec );
+  }
+  */
+
+  /*
+  for( i = 0 ; i < framelen ; i++ ) {
+    printf( "%d %lf\n", i, xr[i] );
+  }
+  */
+
+  return( 0 );
+}
---
+++ 3_6_b.c
@@ -0,0 +1,169 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+void DFT( double *xr, double *xi, double *Xr, double *Xi, int N )
+{
+  int k, n;
+  double wr, wi;
+
+  for( k = 0 ; k < N ; k++ ) {
+    Xr[k] = Xi[k] = 0.0;
+    for( n = 0 ; n < N ; n++ ) {
+      wr = cos( 2.0*M_PI*n*k/N );  wi = sin( 2.0*M_PI*n*k/N );
+      Xr[k] +=  xr[n]*wr + xi[n]*wi;   /* ??????????, using wr & wi */
+      Xi[k] += -xr[n]*wi + xi[n]*wr;   /* ??????????, using wr & wi */
+    }
+  }
+  return;
+}
+
+void IDFT( double *Xr, double *Xi, double *xr, double *xi, int N )
+{
+  int k, n;
+  double wr, wi;
+
+  for( n = 0 ; n < N ; n++ ) {
+    xr[n] = xi[n] = 0.0;
+    for( k = 0 ; k < N ; k++ ) {
+      wr = cos( 2.0*M_PI*n*k/N );  wi = sin( 2.0*M_PI*n*k/N );
+      xr[n] += Xr[k]*wr - Xi[k]*wi;   /* ??????????, using wr & wi */
+      xi[n] += Xr[k]*wi + Xi[k]*wr;   /* ??????????, using wr & wi */
+    }
+    xr[n] /= N;  xi[n] /= N;
+  }
+  return;
+}
+
+int main( int argc, char *argv[] )
+{
+  int  i, nskip, framelen;
+  double spec;
+  short  *sdata;
+  double *xr, *xi, *Xr, *Xi;
+  FILE *fpDAT;
+  long fsize; /*filesize*/
+  
+  if( 4 != argc ) {
+    fprintf( stderr, "Usage : %s DATfile skip[sample] frame_length[sample]\n", argv[0] );
+    exit( 1 );
+  }
+
+  if( NULL == ( fpDAT = fopen( argv[1], "r" ) ) )  exit( 1 );
+  if( 0 > ( nskip    = atoi( argv[2] ) ) )  exit( 1 );
+  if( 0 > ( framelen = atoi( argv[3] ) ) )  exit( 1 );
+
+  fprintf( stderr, "# DATfile = %s\n", argv[1] );
+  fprintf( stderr, "# %d samples are skipped.\n", nskip );
+  fprintf( stderr, "# 1 frame contains %d sampels.\n", framelen );
+
+  /* memory allocation */
+  sdata = (short*)calloc( sizeof(short), framelen );
+  xr = (double*)calloc( sizeof(double), framelen );
+  xi = (double*)calloc( sizeof(double), framelen );
+  Xr = (double*)calloc( sizeof(double), framelen );
+  Xi = (double*)calloc( sizeof(double), framelen );
+  if( NULL == sdata || NULL == xi || NULL == xi || NULL == Xr || NULL == Xi )  exit( 1 );
+
+  fseek(fpDAT, 0, SEEK_END);
+  fsize = ftell(fpDAT);
+  //printf("fsize = %ld\n", fsize);
+  i = 0;
+  /*for(nskip = 0; nskip < fsize; nskip += framelen/2){
+    i++;
+  }
+  */
+  const int shift = i;
+  int count = 0;
+  /*ここから繰り返し*/
+  /* for(nskip = 0; nskip < fsize; nskip += framelen/2){*/
+
+  fseek( fpDAT, nskip*sizeof(short), SEEK_SET );
+  fread( sdata, sizeof(short), framelen, fpDAT );
+  fclose( fpDAT );
+
+  /*
+    for(i = 0; i < framelen; i++){
+    printf("%lf %d\n", i/16000.0, sdata[i]);
+    }
+  */
+  for( i = 0 ; i < framelen ; i++ ) {
+    xr[i] = (0.54-0.46*cos(2*M_PI*i/(framelen-1)))*sdata[i];
+    xi[i] = 0.0;
+  }
+
+  /*
+  for(i = 0; i < framelen; i++){
+    printf("%lf %lf\n", i/16.0, xr[i]);
+  }
+  */
+
+  int t;
+
+  double *rr = (double*)calloc( sizeof(double), framelen );
+  double *vr = (double*)calloc( sizeof(double), framelen );
+  double max = -1;
+  int tau;
+  for(i = 0; i < framelen; i++){
+    /*r[i] = stst+1*/
+    for(t = 0; t < framelen; t++){
+      rr[i] += xr[t]*xr[(t+i)%framelen];
+    }
+
+    vr[i] = rr[i]/framelen;
+
+    /*if(i != 0 && max < fabs(v[i]) && fabs(v[i-1]) < fabs(v[i])){
+      max = fabs(v[i]);
+      tau = i;
+      
+      }
+    */
+  }
+ 
+
+  /*
+    double pitch = log10(1.0/tau);
+    printf("%d %lf\n", count, pitch);
+    count++;
+  */
+  /*
+    for(i = 0; i < framelen; i++){
+    printf("%lf %lf\n", i/16.0, v[i]);
+    }
+  */
+  /* }*/
+
+  //fclose( fpDAT );
+  double * vi = (double*)calloc(sizeof(double), framelen);
+  for(i = 0; i < framelen; i++){
+    vi[i] = 1;
+  }
+
+  /* for ( i = 0 ; i < 1000 ; i++ ) { */
+  DFT( vr, vi, Xr, Xi, framelen );
+  /* } */
+
+  /*
+    IDFT( Xr, Xi, xr, xi, framelen );
+  */
+  /*
+    for( i = 0 ; i < framelen ; i++ ) {
+    spec = log10( (1.0/framelen)*( Xr[i]*Xr[i]+Xi[i]*Xi[i] ) );
+    printf( "%d %lf\n", i, spec );
+    }
+  */
+  
+  for(i = 0; i < framelen; i++){
+    spec = log10(Xr[i]);
+    printf("%d %lf\n", i, spec);
+  }
+  
+  /*
+    for( i = 0 ; i < framelen ; i++ ) {
+    printf( "%d %lf\n", i, xr[i] );
+    }
+  */
+
+  return( 0 );
+}
